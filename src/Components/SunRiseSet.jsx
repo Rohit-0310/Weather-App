a0e0e33af6843e94520931098a14d8d1b9ce52ea
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-11 11:55:19 +0530

    Sunset Sunrise

--- src/Components/SunRiseSet.jsx
+++ src/Components/SunRiseSet.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./SunRiseSet.css"
+import {formatToLocalTime} from "./API/weather"
 const SunRiseSet = ({
     weather,
-    weather: { sunrise, sunset, humidity, pressure},
+    weather: { sunrise, sunset, humidity, pressure ,timezone},
 
 
 }) => {
@@ -16,19 +17,19 @@ const SunRiseSet = ({
                         <span>{pressure} hpa</span>
                     </div>
                     <div>
-                        <span>Humidity</span>
+                        <span className="Bold">Humidity</span>
                         <span>{humidity} %</span>
                     </div>
                 </div>
 
-                <div>
+                <div className="SunTime">
                     <div>
-                        <span>Pressure</span>
-                        <span>{pressure} hpa</span>
+                        <span className="Bold">Sunrise</span>
+                        <span>{formatToLocalTime(sunrise, timezone, "h:mm")}am</span>
                     </div>
                     <div>
-                        <span>Humidity</span>
-                        <span>{humidity} %</span>
+                        <span className="Bold">Sunset</span>
+                        <span>{formatToLocalTime(sunset, timezone, "h:mm")}pm</span>
                     </div>
                 </div>
             </div>
