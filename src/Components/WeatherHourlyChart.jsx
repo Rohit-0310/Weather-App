180f7ad6927527ec119244cdbe2a073c73be20a8
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-11 12:20:33 +0530

    hourly chart

---
+++ src/Components/WeatherHourlyChart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./WeatherHourlyChart.css";
+
+
+
+const WeatherHourlyChart = () => {
+  return (
+    <div>WeatherHourlyChart</div>
+  )
+}
+
+export default WeatherHourlyChart
