77ec5da699762d699875aaa3627ffbecebe907ad
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-11 17:29:18 +0530

    fix error

--- src/Components/API/weather.jsx
+++ src/Components/API/weather.jsx
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "cc213b4fa71c282538f1c25928e79c19";
+const API_KEY = "cc91affcab8e122d30980dc01bd6a879";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
