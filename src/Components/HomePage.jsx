a874a6cf0e6d4976348cacc87640bcbb34183a58
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-11 10:12:46 +0530

    API fetch

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
-import Search from './Search'
-import "./HomePage.css"
-import WeatherDetails from './WeatherDetails'
+import React, { useEffect, useState } from 'react';
+import Search from './Search';
+import WeatherDetails from './WeatherDetails';
+import WeatherWeekList from './WeatherWeekList';
+import "./HomePage.css";
+import getFormattedWeatherData from "./API/weather";
+
 const HomePage = () => {
+
+    const [weather, setWeather] = useState(null);
+    const [query, setQuery] = useState({ q: "Patna" });
+
+    
+    useEffect(() => {
+        const fetchWeather = async () => {
+            await getFormattedWeatherData({ ...query, units: "metric" }).then(
+                (data) => {
+                    setWeather(data);
+                }
+            );
+        };
+        
+        fetchWeather();
+
+        }, [query]);
+
+
+    console.log(weather)
     return (
         <div>
             <div className="home_top">
                 <div className="card">
     
-                    <Search/>
+                    <Search setQuery={setQuery}/>
                     <p>this is for testing purposes</p>
                     <h1>1</h1>
                     <h1>1</h1>
                     <h1>1</h1>
                     <p>1</p>
-                    <p>this is for testing purposes</p>
-                    <h1>1</h1>
-                    <h1>1</h1>
-                    <h1>1</h1>
-                    <p>1</p>
+                    
 
-                    <WeatherDetails />
+                    {weather && (
+                        <div>
+
+                            <WeatherWeekList weather={weather} />
+                            <WeatherDetails weather={weather}/>
+                        </div>
+                    )}
+
                 </div>
             </div>
         
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,21 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import pinloc from "../ICON/pin.png";
 import searchIcon from "../ICON/search-interface-symbol.png"
 import "./Search.css"
-const Search = () => {
-  return (
-    <div>
-        <div>
-            <div className="SearchMain">
-                <div> <img className="LocIcon"  src={pinloc} alt="location" /></div>
-                <input className="InputSearch" type="text" placeholder="Search City" />
-                <button className="SearchButton" type="button">
-                    <img className="LocIcon" src={searchIcon} alt="search" />
-                </button>
-            </div>
-        </div>
-    </div>
-  )
+
+
+
+const Search = ({ setQuery }) => {
+    const [city, setCity] = useState("");
+
+    useEffect(() => {
+        findLocation();
+    }, []);
+
+
+
+    const findLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                function success(position) {
+                    let lat = position.coords.latitude;
+                    let lon = position.coords.longitude;
+                    setQuery({ lat, lon });
+            },
+            function error(error_message) {
+                console.error(
+                    "An error has occured while retrieving location",
+                    error_message
+                );
+            }
+            );
+        } else {
+            console.log("geolocation is not enabled on this browser");
+        }
+    };     
+    
+        const handleKeyDown = (e) => {
+            if (e.key === "Enter") {
+                if (city !== "") {
+                  setQuery({ q: city });
+                }
+                setCity("");
+                // console.log(city);
+            }
+        };
+    return (
+      <div>
+          <div>
+              <div className="SearchMain">
+                    <div> <img className="LocIcon"  src={pinloc} alt="location" /></div>
+
+                    <input className="InputSearch"
+                    type="search"
+                    name="search"
+                    value={city}
+                    onChange={(e)=>setCity(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Search City" 
+                    />
+
+                    <button className="SearchButton" type="button">
+                        <img className="LocIcon" src={searchIcon} alt="search" />
+                    </button>
+              </div>
+          </div>
+      </div>
+    )
 }
 
 export default Search
